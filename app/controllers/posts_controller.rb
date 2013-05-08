7287a235fbb9cca629892b75a2466e7f371f49b9
Olexandr Bondarchuk <alex.coockoo@gmail.com>
2013-05-08 16:35:32 +0300

    Initial posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  def show
+    @post = Post.where(id: params[:id])
+  end
+  def new
+    @post = Post.new(params[:post])
+  end
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+  end
+  def edit 
+    @post = Post.where(id: params[:id])
+  end
+  def update 
+    @post = Post.where(id: params[:id])
+    @post.save
+  end
+  def destroy
+    post = Post.where(id: params[:id])
+    post.destroy
+  end
+  
 end
