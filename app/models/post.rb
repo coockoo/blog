81a223d7476299536fe8dcb972c8c7b89e6069a2
Olexandr Bondarchuk <alex.coockoo@gmail.com>
2013-05-13 00:11:31 +0300

    Added shortened messages to the index

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,11 @@ class Post < ActiveRecord::Base
   def posted_at
     return self.created_at.getlocal
   end
+  def short_post
+    if self.post.length > 100
+      (self.post.index('[code')?self.post.slice(0..self.post.index('[code')-3):self.post.slice(0..100)) + "..."
+    else
+      self.post
+    end
+  end
 end
